7d0a61a3d672ec7779d0206273af099fff6ad595
savinoda <dambrasavio@gmail.com>
2017-03-16 16:13:12 +0100

    DASH SRD manifest creation
    -Copyright notice added
    -Check on tiles coordinates (parameters must range between 0 and 1) added
    -Tiles segmentation added
    -DASH SRD manifest creation added

--- Script/script/tiling_v0.sh
+++ Script/script/tiling_v0.sh
@@ -1,3 +1,5 @@
+#Copyright 2017 Laboratoire I3S, CNRS, Université côte d'azur
+#Author: Savino Dambra
 #!/bin/bash
 #Defining some general purpose variables
 RED='\033[0;31m'
@@ -5,6 +7,7 @@ INTEGERREGEX='^[0-9]+$'
 STARTENC="Start encoding..."
 ENDENC="Encoding complete!\n"
 ENCTEMPFILE="enctempdata.csv"
+DASHTEMPFILE="dashtempfile.sh"
 
 #Removing temp data if exists
 if [ -f "$ENCTEMPFILE" ]; then
@@ -31,6 +34,19 @@ if [ -z "$4" ]; then
         exit 1
 fi
 echo -e "Output file name <- ${4}!"
+
+#Checking whether the number of second each segment is composed of is provided
+if [ -z "$5" ]; then
+        #Value not provided: keeping the default one
+        slicing=5000
+else
+        slicing=$5 
+fi
+#Initializing the temp file for SECTION2
+DIR=$(dirname "${4}")
+OUT=$(basename "${4}")
+mkdir $DIR/$OUT
+echo -e "MP4Box -dash ${slicing} -segment-name %s_ -out ${DIR}/${OUT}/manifest.mpd \\" >> $DASHTEMPFILE
 
 ##SECTION 0 - VIDEO TRANSCODING AT MULTIPLE RESOLUTIONS/BITRATES
 #Checking whether the file path is provided:
@@ -82,7 +98,7 @@ else
 					echo -e "\t${width}\t${height}\t${bitrate}k"
 					#Start encoding...
                                 	echo -e "${STARTENC}"
-					outputfilename=${4}_${bitrate}k_${width}x_${height}
+					outputfilename=${4}_${bitrate}k_${width}x${height}
 					ffmpeg -y -i $1 \
 					-vf scale=${width}:${height} \
 					-b:v ${bitrate}k -bufsize ${bitrate}k \
@@ -97,7 +113,7 @@ else
 					echo -e "\t${width}\t${height}\tSrc"
 					#Start encoding...
 					echo -e "${STARTENC}"
-					outputfilename=${4}_${width}x_${height}
+					outputfilename=${4}_${width}x${height}
                                 	ffmpeg -y -i $1 \
                                         -vf scale=${width}:${height} \
                                         -loglevel 16 -hide_banner \
@@ -130,26 +146,49 @@ else
                 #File exists. Computing the tiling for each transcoded video
                 cat $ENCTEMPFILE | while IFS=, read  first second third
                 do
-			echo -e "Actual video: ${first}"
+			echo -e "Current video: ${first}.mp4"
+			line=0
 			cat $3 | while IFS=, read  startwidth startheight endwidth endheight
 			do
-				startwpixel=$(echo "$second*$startwidth" |bc)
-				starthpixel=$(echo "$third*$startheight" |bc)
-				endwpixel=$(echo "$second*$endwidth" |bc)
-				endhpixel=$(echo "$third*$endheight" |bc)
-				areawidth=$(echo "$endwpixel-$startwpixel" |bc)
-				areaheight=$(echo "$endhpixel-$starthpixel" |bc)
-				#Start cropping
-				echo -e "\tCropping from (${startwpixel},${starthpixel}) to (${endwpixel},${endhpixel}) with resolution (${areawidth},${areaheight})..."
-				outputfilename="${first}_(w-${areawidth},h-${areaheight})_(sp-${startwpixel},${starthpixel})"
-				ffmpeg -y -i $first.mp4 -filter:v "crop=${areawidth}:${areaheight}:${startwpixel}:${starthpixel}" \
-				-loglevel 16 -hide_banner -c:a copy \
-				$outputfilename.mp4 \
-				< /dev/null \
-				#Cropping complete
-				echo -e "\tCropping complete"
+				#Checking if the coordinates are expressed as percentage between 0 and 1
+				if (( \
+				$(echo "$startwidth <= 1" | bc -l) &&  $(echo "$startwidth >= 0" | bc -l) \
+				&& $(echo "$startheight <= 1" | bc -l) &&  $(echo "$startheight >= 0" | bc -l) \
+				&& $(echo "$endwidth <= 1" | bc -l) &&  $(echo "$endwidth >= 0" | bc -l) \
+				&& $(echo "$endheight <= 1" | bc -l) &&  $(echo "$endheight >= 0" | bc -l) \
+				)); then
+					startwpixel=$(echo "$second*$startwidth" |bc)
+					starthpixel=$(echo "$third*$startheight" |bc)
+					endwpixel=$(echo "$second*$endwidth" |bc)
+					endhpixel=$(echo "$third*$endheight" |bc)
+					areawidth=$(echo "$endwpixel-$startwpixel" |bc)
+					areaheight=$(echo "$endhpixel-$starthpixel" |bc)
+					#Start cropping
+					echo -e "\tCropping from (${startwpixel},${starthpixel}) to (${endwpixel},${endhpixel}) with resolution (${areawidth},${areaheight})..."
+					outputfilename="${first}_w-${areawidth},h-${areaheight}_sp-${startwpixel},${starthpixel}"
+					ffmpeg -y -i $first.mp4 -filter:v "crop=${areawidth}:${areaheight}:${startwpixel}:${starthpixel}" \
+					-loglevel 16 -hide_banner -c:a copy \
+					$outputfilename.mp4 \
+					< /dev/null \
+					#Cropping complete
+					echo -e "\tCropping complete"
+					#Adding this tile to a file for the next section
+					echo -e "${outputfilename}.mp4:desc_as='<SupplementalProperty schemeIdUri="urn:mpeg:dash:srd:2014" value="0,${startwidth},${startheight},${endwidth},${endheight},1,1"/>' \\" >> $DASHTEMPFILE
+				else
+					#Skip the line
+					echo  -e "\tValues not between 0 and 1 at the line $((line+1)). Line has been skipped!"
+				fi
+			line=$((line+1))
 			done
 		done
 	fi
 fi
-echo "Process completed!"
+##SECTION 2 - DASH AND SRD
+#Launching the script produced in the section 1
+echo "Segmenting each tile and creating the DASH-SRD representation..."
+./${DASHTEMPFILE}
+echo "DASH-SRD representation created!"
+#Removing temp files
+rm $DASHTEMPFILE
+#Process completed
+echo "Overall process completed!"
