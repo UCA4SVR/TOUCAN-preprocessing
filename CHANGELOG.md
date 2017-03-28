ae1aacdb66c2712da64e1e9b683df7aae7dd8c57
Romaric Pighetti <romaric.pighetti@gmail.com>
2017-03-28 15:56:52 +0200

    Added a changelog and applied the license properly

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## V 1.0.0
++ First stable version of the preprocessing scripts
++ Readme providing short explanations
++ Wiki providing more detailed explanations
++ One script to transcode, build several resolutions and bitrates and tile the video
++ One script to only tile the video
++ Both scripts output the manifest mpd file in the DASH-SRD format
--- Script/script/tiling_only.sh
+++ Script/script/tiling_only.sh
@@ -1,6 +1,18 @@
-#Copyright 2017 Laboratoire I3S, CNRS, Université côte d'azur
-#Author: Savino Dambra
 #!/bin/bash
+# Copyright 2017 Laboratoire I3S, CNRS, Université côte d'azur
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Author: Savino Dambra
+
 #Defining some general purpose variables
 RED='\033[0;31m'
 INTEGERREGEX='^[0-9]+$'
@@ -36,7 +48,7 @@ if [ -z "$4" ]; then
         #Value not provided: keeping the default one
         slicing=5000
 else
-        slicing=$4 
+        slicing=$4
 fi
 #Initializing the temp file for SECTION2
 DIR=$(dirname "${3}")
